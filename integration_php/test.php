4fbebf5db381498bd3735f580759a2f32f59906a
Filipe Mateus Nascimento <filipemateusn@gmail.com>
2017-01-18 10:33:45 -0200

    Fix PHP example

--- integration_php/test.php
+++ integration_php/test.php
@@ -5,7 +5,7 @@ require_once('RD_Station.php');
 $rdstation = new RD_Station($_POST);
 $rdstation->token = 'Aec5waPho3AhFeez7wue0Oopohcheich';
 $rdstation->identifier = 'Formulário de Contato';
-$rdstation->ignore_fields = (['password', 'password_confirmation', 'captcha']);
+$rdstation->ignore_fields(['password', 'password_confirmation', 'captcha']);
 $rdstation->redirect_success = 'test.php';
 $rdstation->redirect_error = 'test.php';
 $rdstation->createLead();
